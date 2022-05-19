9563c38293add0879b798a4a4964c8b5fae27ab3
M R Kishore Kumar <mrkishorekumar18@gmail.com>
2022-05-19 16:00:48 +0530

    upload

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function encoding(){
     document.getElementById('encodingResult').innerHTML = btoa(get);
 }
 
+function Copy1(){
+    var copyText = document.getElementById("encodingResult");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); 
+    navigator.clipboard.writeText(copyText.value)
+}
+
 function Copy(){
     var copyText = document.getElementById("fileout");
     copyText.select();
